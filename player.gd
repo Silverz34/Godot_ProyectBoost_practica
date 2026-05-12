97a3c2ada264598ce5b594a1e205f8164a2e2c26
Mishell <pradogordillomichael@gmail.com>
2026-05-11 23:43:17 -0600

    manejo de variables globales, uso de @xport para manejo de interfaz

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
 
 # variable global 
-var thrust: float = 1000.0 
-var rotate_speed: float = 100.0
+@export_range(750.0, 3000.0) var thrust: float = 1000.0 
+@export_range(50.0, 150.0) var torque_thrus: float = 100.0 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,14 +10,19 @@ func _process(delta: float) -> void:
 		apply_central_force(basis.y * delta * thrust)
 		
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, rotate_speed * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrus * delta))
 		
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -rotate_speed * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrus * delta))
 
 func _on_body_entered(body: Node) -> void: 
 	if "Goal" in body.get_groups():
 		print("you win")
 
 	if "Hazard" in body.get_groups():
-		print("you crashed!")
+		crash_secuence()
+
+func crash_secuence() -> void:
+	print("KABOOM!")
+	get_tree().reload_current_scene()
+	
