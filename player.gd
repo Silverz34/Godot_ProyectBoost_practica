9688d6aaa7e4a2726b093da23b0617278bb4ea0d
Mishell <pradogordillomichael@gmail.com>
2026-05-11 23:56:38 -0600

    ajustes de recarga de escenas y funcion rigida sobre el movimiento en x y Y del cohete

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ extends RigidBody3D
 @export_range(50.0, 150.0) var torque_thrus: float = 100.0 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
 		
@@ -22,7 +22,9 @@ func _on_body_entered(body: Node) -> void:
 	if "Hazard" in body.get_groups():
 		crash_secuence()
 
+#funcion para recheckpoit al momento de estrellarse con la base 
 func crash_secuence() -> void:
 	print("KABOOM!")
-	get_tree().reload_current_scene()
+	#Recalculo de choque en la escena.
+	get_tree().call_deferred("reload_current_scene")
 	
