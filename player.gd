5c5d7c94104a3042cd574ec8ed0e6d21e359c650
Mishell <pradogordillomichael@gmail.com>
2026-05-13 12:02:38 -0600

    uso de tween para mejorar un poco la animacion en cuanto se estrella el cuerpo del cohete

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0 
 @export_range(50.0, 150.0) var torque_thrus: float = 100.0 
 
+var is_transition: bool = false 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -16,17 +18,25 @@ func _physics_process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrus * delta))
 
 func _on_body_entered(body: Node) -> void: 
-	if "Goal" in body.get_groups():
-		complete_level(body.file_path)
+	if is_transition == false: 
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
 
-	if "Hazard" in body.get_groups():
-		crash_secuence()
+		if "Hazard" in body.get_groups():
+			crash_secuence()
 
 #funcion para recheckpoit al momento de estrellarse con la base 
 func crash_secuence() -> void:
 	print("KABOOM!")
+
+	set_process(false)
+	is_transition = true
+	
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 	#Recalculo de choque en la escena.
-	get_tree().call_deferred("reload_current_scene")
+	
 	
 func complete_level(next_level_path: String) -> void:
 	print("level completed")
